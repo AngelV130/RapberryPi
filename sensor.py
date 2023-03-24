6d7655062925d974ffc34bc1aa0cb3995e7f2250
Angel Vargas <21170057@uttcampus.edu.mx>
2023-03-24 14:39:35 -0600

    first commit

--- sensor.py
+++ sensor.py
@@ -167,7 +167,31 @@ class Sensor:
                     lista.agregar(sensorData)
                     break; #Cada segundo se evalúa el sensor
         lista.guardar(lista.lista);
-
+    @staticmethod
+    def led():
+        pin = 12
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(pin, GPIO.LOW)
+        res = ""
+        while res != "si":
+            print("1-Apagar led")
+            print("2-Encender led")
+            opcion = input("Que deseas hacer?")
+            try:
+                opcion = int (opcion)
+            except:
+                print("eso no es un numero")
+            if opcion == 1:
+                GPIO.output(pin, GPIO.LOW)
+            elif opcion == 2:
+                GPIO.output(pin, GPIO.HIGH)
+            else:
+                print("Opcion no valida")
+            res = input("deseas salir?")
+        GPIO.cleanup()
 
 
 #Sensor.mostrarSensores()
